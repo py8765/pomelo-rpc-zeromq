3168d37670428fca52247dd1ecfdc4fc7da2389f
palmtoy <zgli@corp.netease.com>
2014-02-12 11:39:31 +0800

    * send 3 msgs

--- sample/client.js
+++ sample/client.js
@@ -29,10 +29,10 @@ var routeFunc = function(routeParam, msg, routeContext, cb) {
 var client = Client.create({routeContext: routeContext, router: routeFunc, context: context});
 
 // var sum = 10000
-var sum = 300
+var sum = 3
   , retNum = 0
   // , sendInterval = 1;
-  , sendInterval = 100;
+  , sendInterval = 10;
 
 client.start(function(err) {
   console.log('rpc client start ok.');
