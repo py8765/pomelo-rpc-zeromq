c3ab8751fedee8e4ba29664ca0995461d6b86151
palmtoy <zgli@corp.netease.com>
2014-02-12 10:03:28 +0800

    * use domain

--- sample/server.js
+++ sample/server.js
@@ -5,8 +5,8 @@ var paths = [
   {namespace: 'user', path: __dirname + '/remote/test'}
 ];
 
-// var host = 'pomelo3.server.163.org';
-var host = '10.120.144.102';
+// var host = '10.120.144.102';
+var host = 'pomelo3.server.163.org';
 var port = 3333;
 
 var server = Server.create({paths: paths, port: port, host: host});
