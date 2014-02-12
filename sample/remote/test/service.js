287a7f541ac7b6075f3793c1af80312290db7b13
palmtoy <zgli@corp.netease.com>
2014-02-12 10:18:15 +0800

    * modify echo content

--- sample/remote/test/service.js
+++ sample/remote/test/service.js
@@ -3,7 +3,8 @@
 module.exports = function(context) {
   return {
     echo: function(msg, cb) {
-      cb(null, 'echo: ' + msg);
+      // cb(null, 'echo: ' + msg);
+      cb(null, 200);
     }
   };
 };
