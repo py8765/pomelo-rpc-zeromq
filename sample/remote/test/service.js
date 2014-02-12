5d658f41571ac57d79999c7f6fe44fc2957d00aa
palmtoy <zgli@corp.netease.com>
2014-02-12 15:00:42 +0800

    * calc svr time

--- sample/remote/test/service.js
+++ sample/remote/test/service.js
@@ -1,8 +1,19 @@
 // remote service
+var maxNum = 19970;
+var curNum = 0;
+var beginTime = 0;
+
 
 module.exports = function(context) {
   return {
     echo: function(msg, cb) {
+      if(beginTime === 0) {
+        beginTime = Date.now();
+      }
+      ++curNum;
+      if(curNum > maxNum) {
+        console.error('%d ~ All RPC costTime = %d(ms)', curNum, (Date.now() - beginTime));
+      }
       // cb(null, 'echo: ' + msg);
       cb(null, 200);
     }
